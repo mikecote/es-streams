159abba5db38072f30162b18416232f2c8d5c12a
Mike Côté <mikecote1990@gmail.com>
2018-07-07 17:04:47 -0400

    Add more exports in /src

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+const BulkStream = require('./BulkStream.js');
+const DeleteStream = require('./DeleteStream.js');
 const IndexStream = require('./IndexStream.js');
 const LiveSearchStream = require('./LiveSearchStream.js');
 const ScrollSearchStream = require('./ScrollSearchStream.js');
+const UpdateStream = require('./UpdateStream.js');
 
 module.exports = {
+	BulkStream,
+	DeleteStream,
 	IndexStream,
 	LiveSearchStream,
 	ScrollSearchStream,
+	UpdateStream,
 };
